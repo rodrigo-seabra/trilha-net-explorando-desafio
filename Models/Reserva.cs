9312a561f04a9ff7b37494dc21faada5a52cd659
rodrigo-seabra <rodrigo.seabra08@outlook.com>
2024-04-12 13:20:36 -0300

    Desafio resolvido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,18 +16,25 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite == null)
+            {
+                throw new ArgumentException("A suíte não foi cadastrada.");
+            }
+
+            if (hospedes == null)
+            {
+                throw new ArgumentNullException(nameof(hospedes), "A lista de hóspedes não pode ser nula.");
+            }
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new ArgumentException("O número de hóspedes não pode ser maior que a capacidade da suite.");
             }
         }
-
         public void CadastrarSuite(Suite suite)
         {
             Suite = suite;
@@ -36,23 +43,23 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            if (Hospedes == null)
+            {
+                return 0;
+            }
+
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite == null)
             {
-                valor = 0;
+                throw new ArgumentException("A suíte não foi cadastrada.");
             }
+
+            decimal valor = DiasReservados * Suite.ValorDiaria * (DiasReservados >= 10 ? 0.9M : 1);
 
             return valor;
         }
